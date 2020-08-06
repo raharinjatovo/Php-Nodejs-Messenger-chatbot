44196ccf3e3c7bd9dda680170edaad70754f23ca
mbolatiana <raharinjatovo.a@zurcher.edu.mg>
2020-08-06 11:37:40 +0300

    first commit

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+1
+set your webhook in developers.facebook.com
+
+use fb_webhook.php to validate webhook
+
+2/
+use data.js to get the webhook and send respond by node js 
+
---
+++ data.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const express = require('express');
+const https = require('https');
+const http = require('http');
+
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+// schedule instruction
+let token ='youtoken'
+
+
+app.get('/webhook/', function(req, res) {
+  let  datarecieved = req.url.substr(10,1000);
+let parsing = datarecieved.replace(/%22/g, '"');
+
+
+ 
+let json = JSON.parse(parsing);
+	let messaging_events = json.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text.replace(/%100/g, ' ')
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
+
+app.listen(app.get('port'), function() {
+	console.log("running: port")
+})
